726739a5c5b179888ee8491709543d0c104addfb
KinderBuilder <hypersnova101@gmail.com>
2021-07-03 10:06:23 -0800

    worked on the client side of server

---
+++ client/Post.java
@@ -0,0 +1,34 @@
+//create to json
+public class Post{
+    private String title;
+    private String sender;
+    private String body;
+    public Post(){}
+    public Post(String s, String t, String b){
+        this.sender = s;
+        this.body = b;
+        this.title = t;
+    }
+    public String getBody(){
+        return body;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getSender(){
+        return sender;
+    }
+    public void setBody(String body){
+        this.body = body;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setSender(String sender){
+        this.sender = sender;
+    }
+    @Override
+    public String toString(){
+        return "Title: " + this.title + "\nBody: " + this.body + "\nSent by:" + this.sender + "\n";
+    }
+}
